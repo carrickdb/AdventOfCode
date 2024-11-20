311bf0737fff0de2b51404a6eaf4f556f4284877
Carrick Bartle <carrickdb@gmail.com>
2024-11-19 19:33:26 -0800

    2017 day 1

--- aoc.py
+++ aoc.py
@@ -1,6 +1,13 @@
 dirs = [[0,1], [0,-1], [-1, 0], [1,0]]
 diagDirs = [[0,1], [0,-1], [-1, 0], [1,0], [-1, -1], [1, 1], [-1, 1], [1, -1]]
 
+def getInput(filename):
+    input = []
+    with open(filename) as f:
+        for l in f:
+            input.append(l.strip())
+    return input
+
 def printMapGrid(g):
     for i in sorted(g.keys(), reverse=True):
     # for i in sorted(g.keys()):
@@ -12,13 +19,6 @@ def printGridList(g):
     for row in g:
         print(''.join(row))
     print()
-
-def getInput(filename):
-    input = []
-    with open(filename) as f:
-        for l in f:
-            input.append(l.strip())
-    return input
 
 def gridify(input):
     g = {}
