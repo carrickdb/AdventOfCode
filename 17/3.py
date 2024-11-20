313fd0b9c93582ffcfa71c335f762d2fde88413b
Carrick Bartle <carrickdb@gmail.com>
2024-11-19 21:24:23 -0800

    day 3 part 1 2017

---
+++ 17/3.py
@@ -0,0 +1,22 @@
+import math
+
+input = [12, 23, 1024]
+
+input = [277678]
+
+
+
+
+# for i in input:
+#     sr = int(math.sqrt(i))
+#     if sr%2 == 0:
+#         sr += 1
+#     elif sr*sr != i:
+#         sr += 2
+
+#     position = i - pow(sr-2, 2) - 1
+#     part = position//(sr-1)
+#     order = position%(sr-1)
+#     ring = sr//2-1
+
+#     print(sr//2 + abs(order - ring))
