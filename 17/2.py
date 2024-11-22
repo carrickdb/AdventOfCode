ac52245e7ede7446e92902dc46beced5592f9873
Carrick Bartle <carrickdb@gmail.com>
2024-11-21 20:58:57 -0800

    2017 day 3 part 2

--- 17/2.py
+++ 17/2.py
@@ -2,7 +2,6 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import aoc
-from functools import reduce
 
 # input = aoc.getInput("test-input")
 input = aoc.getInput("input")
