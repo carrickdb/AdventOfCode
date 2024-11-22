5d65930870900cdb59c732b1e3ea33f99b137eb0
Carrick Bartle <carrickdb@gmail.com>
2024-11-21 22:37:15 -0800

    2019 day 2 part 1

--- 19/2.py
+++ 19/2.py
@@ -1,7 +1,24 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import aoc
 
 
-with open("input") as f:
-    for line in f:
-        program = line.strip().split(',')
-        program = list(map(int, program))
+# input = aoc.getInput("test-input")
+input = aoc.getInput("input")
 
+m = {
+    1: lambda x, y: x+y,
+    2: lambda x,y: x*y,
+}
+for l in input:
+    l = list(map(int, l.split(",")))
+    l[1] = 12
+    l[2] = 2
+    for i in range(0, len(l), 4):
+        op = l[i]
+        if op == 99:
+            break
+        fun = m[l[i]]
+        l[l[i+3]] = fun(l[l[i+1]], l[l[i+2]])
+    print(l[0])
