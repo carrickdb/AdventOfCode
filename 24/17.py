437e996be577f6feb6215a04f2e0be2b104be3be
Carrick Bartle <carrickdb@gmail.com>
2024-12-22 23:05:36 -0800

    2024 day 16

---
+++ 24/17.py
@@ -0,0 +1,8 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import aoc
+
+input = aoc.getInput("input")
+# input = aoc.getInput("test-input")
+# input = aoc.getInput("second-example")
