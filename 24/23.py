852d4895ef59084abc3a5817749db783063fd5c1
Carrick Bartle <carrickdb@gmail.com>
2024-12-30 10:28:42 -0800

    day 22

---
+++ 24/23.py
@@ -0,0 +1,9 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import aoc
+from collections import defaultdict
+
+# stuff = aoc.getInput(os.getenv("HOME") + "/Desktop/input")
+stuff = aoc.getInput("test-input")
+
